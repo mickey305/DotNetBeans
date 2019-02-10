f834eca5ba9267e8f911346af9e115f86856690b
K.Misaki <pro.mickey.12091it@gmail.com>
2019-02-10 21:43:05 +0900

    不要なUsingを削除、アセンブリ情報を更新

--- DotNetBeans/Properties/AssemblyInfo.cs
+++ DotNetBeans/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("DotNetBeans")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(".Net application's common library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DotNetBeans")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2019 K.Misaki")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
